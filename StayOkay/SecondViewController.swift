551746a464fe9d3eb68432ffbdb04c9c16a117bb
Ava Deguzman <avamdeguzman@gmail.com>
2020-07-29 14:09:01 -0700

    2:08 Push

--- StayOkay/SecondViewController.swift
+++ StayOkay/SecondViewController.swift
@@ -10,28 +10,10 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-     struct ContentView: View {
-    @State private var mood: Double = 0
-
-    var body: some View {
-        VStack {
-            Slider(value: $mood, in: -50...50, step: 25)
-            
-            var mood = 26...50
-            
-            if mood == (26...50) {
-                print ("😃") } else if mood == (1...25) { print ("🙂")
-              } print ("😐") } else if mood == (-25...1) { print ("😕") } else if mood == (-50...26) {
-                print ("😔") 
-        }
-    }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+     
+    
         // Do any additional setup after loading the view.
     }
 
 
-}
 

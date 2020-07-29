abb31045042e884abafeb30dc50cf32e5397b74d
MICHELLE LY <michellely04@icloud.com>
2020-07-29 14:52:42 -0700

    button to links

--- StayOkay/ThirdViewController.swift
+++ StayOkay/ThirdViewController.swift
@@ -9,57 +9,36 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+
     
-    @IBAction func linkClicked(sender: AnyObject) {
+    @IBAction func linkClicked(_ sender: Any) {
         openUrl(urlStr: "https://news.yale.edu/2020/07/27/improve-students-mental-health-yale-study-finds-teach-them-breathe")
-    }
+           }
 
-    func openUrl(urlStr:String!) {
-
-         if let url = NSURL(string:urlStr) {
-            UIApplication.shared.open(url as URL)
-    }
-
-   
-    @IBAction func linkClicked2(sender: AnyObject) {
-        openUrl(urlStr: "https://abcnews.go.com/Health/schools-reopen-students-mental-health-experts-weigh/story?id=71969959")
-    }
-
-    func openUrl(urlStr:String!) {
-
-         if let url = NSURL(string:urlStr) {
-            UIApplication.shared.open(url as URL)
-             
-    }  
-        @IBAction func linkClicked2(sender: AnyObject) {
-        openUrl(urlStr: "https://techerati.com/features-hub/opinions/technologys-role-in-improving-mental-health/")
-    }
-
-    func openUrl(urlStr:String!) {
-
-         if let url = NSURL(string:urlStr) {
-            UIApplication.shared.open(url as URL)
-             
-    }  
-       }
-    
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func linkClicked2(_ sender: Any) {
+    openUrl(urlStr: "https://abcnews.go.com/Health/schools-reopen-students-mental-health-experts-weigh/story?id=71969959")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func linkClicked3(_ sender: Any) {
+   openUrl(urlStr: "https://techerati.com/features-hub/opinions/technologys-role-in-improving-mental-health/")
+        
     }
-    */
+    
+    func openUrl(urlStr:String!) {
 
+                if let url = NSURL(string:urlStr) {
+                   UIApplication.shared.open(url as URL)
+            }
+    
+
+
+
+
+
+
+
+
+
+
+}
 }

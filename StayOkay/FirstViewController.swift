018f34da3494109785d0610af9b8b2addc0980b7
Ava Deguzman <avamdeguzman@gmail.com>
2020-07-28 20:28:15 -0700

    Initial Commit

---
+++ StayOkay/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  StayOkay
+//
+//  Created by Ava Deguzman on 7/28/20.
+//  Copyright © 2020 Ava Deguzman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StayOkay/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StayOkay
+//
+//  Created by Ava Deguzman on 7/28/20.
+//  Copyright © 2020 Ava Deguzman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

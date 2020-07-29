cc89156e5276b0e4477b026923a98440ea65964d
MichelleLy04 <68874072+MichelleLy04@users.noreply.github.com>
2020-07-29 14:09:18 -0700

    Update ThirdViewController.swift

--- StayOkay/ThirdViewController.swift
+++ StayOkay/ThirdViewController.swift
@@ -9,6 +9,41 @@
 import UIKit
 
 class ThirdViewController: UIViewController {
+    
+    @IBAction func linkClicked(sender: AnyObject) {
+        openUrl(urlStr: "https://news.yale.edu/2020/07/27/improve-students-mental-health-yale-study-finds-teach-them-breathe")
+    }
+
+    func openUrl(urlStr:String!) {
+
+         if let url = NSURL(string:urlStr) {
+            UIApplication.shared.open(url as URL)
+    }
+
+   
+    @IBAction func linkClicked2(sender: AnyObject) {
+        openUrl(urlStr: "https://abcnews.go.com/Health/schools-reopen-students-mental-health-experts-weigh/story?id=71969959")
+    }
+
+    func openUrl(urlStr:String!) {
+
+         if let url = NSURL(string:urlStr) {
+            UIApplication.shared.open(url as URL)
+             
+    }  
+        @IBAction func linkClicked2(sender: AnyObject) {
+        openUrl(urlStr: "https://techerati.com/features-hub/opinions/technologys-role-in-improving-mental-health/")
+    }
+
+    func openUrl(urlStr:String!) {
+
+         if let url = NSURL(string:urlStr) {
+            UIApplication.shared.open(url as URL)
+             
+    }  
+       }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
